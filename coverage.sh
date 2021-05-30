e3cd4ad3a73a10ad73336826ecfc382644540935
Ivan <ivan.zderadicka@gmail.com>
2021-05-30 17:34:59 +0200

    create coverage action

--- coverage.sh
+++ coverage.sh
@@ -5,7 +5,6 @@ Generates coverage report
 Need to install:
     cargo install rustfilt
     rustup component add llvm-tools-preview
-    cargo publish
     cargo install cargo-binutils
     sudo apt install jq
 EOF
@@ -26,8 +25,11 @@ OBJECTS=$( \
 
 RUST_BACKTRACE=1 RUSTFLAGS="-Zinstrument-coverage"   LLVM_PROFILE_FILE="coverage/coverage-%m.profraw"   cargo test --tests
 cargo profdata -- merge     -sparse coverage/coverage-*.profraw -o coverage/coverage.profdata
-cargo cov -- report --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/'  \
-    --instr-profile=coverage/coverage.profdata $OBJECTS
+# cargo cov -- report --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/'  \
+#     --instr-profile=coverage/coverage.profdata $OBJECTS
 
-# cargo cov -- show     --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/'     --instr-profile=json5format.profdata     --object target/debug/deps/dstream-0cacdb0ba3784a41  --show-instantiations --show-line-counts-or-regions Xdemangler=rustfil
-# cargo cov -- export     --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/'     --instr-profile=json5format.profdata     --object target/debug/deps/dstream-0cacdb0ba3784a41 --summary-only | jq -r .data[0].totals.lines.percent
+# cargo cov -- show     --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/'     \
+# --instr-profile=json5format.profdata     --object target/debug/deps/dstream-0cacdb0ba3784a41  --show-instantiations --show-line-counts-or-regions Xdemangler=rustfil
+
+cargo cov -- export     --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/rustc/' \
+--instr-profile=coverage/coverage.profdata  $OBJECTS --summary-only | jq -r .data[0].totals.lines.percent
